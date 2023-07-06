f2d4c584e568f2387b1c5b9044634be83a3cfb6f
Reginaldo Santos <reginaldosantos.br@gmail.com>
2023-07-05 23:44:05 -0300

    test: ensure AddRoomUsecase throws when data is invalid

--- spec/usecases/add-room.test.ts
+++ spec/usecases/add-room.test.ts
@@ -3,16 +3,23 @@ import { RoomModel } from "../../src/domain/room";
 import { AddRoomUsecase } from "../../src/usecases/add-room";
 
 describe("Add Room Usecase", () => {
-  it("should throw MissingParamError when room number is not provided", () => {
-    const fakeRoomProps = {
-      price: 1,
-      status: "ok",
-    };
+  it("should throw MissingParamError when data is invalid", () => {
+    const testCases = [
+      { price: 1, status: "ok" },
+      { number: 1, status: "ok" },
+      { number: 1, price: -1, status: "ok" },
+      { number: 1, price: 2 },
+      { number: 1, price: 2, status: "invalid-status" },
+    ];
 
     const addRoomUsecase = new AddRoomUsecase();
 
-    expect(() =>
-      addRoomUsecase.execute(fakeRoomProps as RoomModel)
-    ).toThrowError(MissingParamError);
+    testCases.forEach(({ number, price, status }) => {
+      const fakeRoomProps = { number, price, status };
+
+      expect(() =>
+        addRoomUsecase.execute(fakeRoomProps as RoomModel)
+      ).toThrowError(MissingParamError);
+    });
   });
 });
